74cc34f4b13da18f362039236bd710b2e7e72962
Nandhini M <30834130+NandhiniManian@users.noreply.github.com>
2018-03-08 16:07:35 +0530

    Create a string print it without space .c

---
+++ set 2/a string print it without space .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+ char a[10],t[10];
+ int i;
+ printf("enter the string:\n");
+ scanf("%[^\n]s",a);
+ for(i=0;a[i]!='\0';i++)
+ {
+     if(a[i]!=' ')
+     {
+     printf("%c",a[i]);
+     }
+ }
+
+}
