01e1ed75806ccd4f07e29e085896d57ee3f2c188
Nandhini M <30834130+NandhiniManian@users.noreply.github.com>
2018-03-13 15:50:22 +0530

    Create area of triangle .c

---
+++ set 22/area of triangle .c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+int main()
+{
+int height,base,area;
+printf("enter the height and base:\n");
+scanf("%d\t%d\t",&height,&base);
+area=(height*base)/2;
+printf("area of triangle is:%d",area);
+return 0;
+}
