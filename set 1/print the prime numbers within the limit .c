7463cdbfdc98bcbd0fb5df5e73197c805ba9c5b5
Nandhini M <30834130+NandhiniManian@users.noreply.github.com>
2018-03-20 15:48:36 +0530

    Update print the prime numbers within the limit .c

--- set 1/print the prime numbers within the limit .c
+++ set 1/print the prime numbers within the limit .c
@@ -1,19 +1,20 @@
 #include<stdio.h>
 void main()
 {
-int a,b,i,j,c=0;
+int a,b,i,j,c=0,m=0;
 printf("enter the limit values:\n");
 scanf("%d%d",&a,&b);
-for(i=a+1;i<b;i++)
+for(i=a;i<=b;i++)
 {
-for(j=1;j<b;j++)
+count=0;
+for(j=2;j<=b;j++)
 {
 if(i%j==0)
 {
 c++; 
 }
 }
-if(c==2)
-printf("%d",i);
-}
+if(c==0)
+m++;
+}printf("%d",m);
 }
