7ddb8cc2b026146f3a81fb77090af45e726e4831
Nandhini M <30834130+NandhiniManian@users.noreply.github.com>
2018-03-20 16:54:28 +0530

    Create print the most repeated letter .c

---
+++ set 4/print the most repeated letter .c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+	char s[50];
+	int i,j,r,l,count=0,max=0;
+	printf("Enter the string:");
+	scanf("%[^\n]s",s);
+	l=strlen(s);
+	for(i=0;i<l;i++)
+	{
+		for(j=i+1;j<l;j++)
+		{
+			if(s[i]==s[j])
+			{
+				count++;
+			}
+			if(count>max)
+			{
+				r=s[i];
+				max=count;
+			}
+		}
+	}
+	printf("%c",r);
+}
