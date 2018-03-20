83a568ead5f9cd568e5aebfa6520c2995201082f
Nandhini M <30834130+NandhiniManian@users.noreply.github.com>
2018-03-20 16:55:45 +0530

    Update print the most repeated letter .c

--- set 4/print the most repeated letter .c
+++ set 4/print the most repeated letter .c
@@ -2,7 +2,7 @@
 void main()
 {
 	char s[50];
-	int i,j,r,l,count=0,max=0;
+	int i,j,a,l,count=0,max=0;
 	printf("Enter the string:");
 	scanf("%[^\n]s",s);
 	l=strlen(s);
@@ -16,7 +16,7 @@ void main()
 			}
 			if(count>max)
 			{
-				r=s[i];
+				a=s[i];
 				max=count;
 			}
 		}
